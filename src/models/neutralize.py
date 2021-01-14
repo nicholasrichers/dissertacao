7a73cd7085450dfaec2a70ff73f30ac6a05575ad
Nicholas Richers <nicholasrichers@gmail.com>
2021-01-14 19:37:13 -0300

    update pkl

--- src/models/neutralize.py
+++ src/models/neutralize.py
@@ -12,6 +12,113 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 
 
+import torch
+from torch.nn import Linear
+from torch.nn import Sequential
+from torch.functional import F
+
+
+
+def exposures(x, y):
+    x = x - x.mean(dim=0)
+    x = x / x.norm(dim=0)
+    y = y - y.mean(dim=0)
+    y = y / y.norm(dim=0)
+    return torch.matmul(x.T, y)
+
+
+
+def reduce_exposure(prediction, features, max_exp):
+    # linear model of features that will be used to partially neutralize predictions
+    lin = Linear(features.shape[1],  1, bias=False)
+    lin.weight.data.fill_(0.)
+    model = Sequential(lin)
+    optimizer = torch.optim.Adamax(model.parameters(), lr=1e-4)
+
+    feats = torch.tensor(np.float32(features)-.5)
+    pred = torch.tensor(np.float32(prediction))
+    start_exp = exposures(feats, pred[:,None])
+
+    # set target exposure for each feature to be <= current exposure
+    # if current exposure is less than max_exp, or <= max_exp if  
+    # current exposure is > max_exp
+    targ_exp = torch.clamp(start_exp, -max_exp, max_exp)
+
+    for i in range(100000):#100000
+        optimizer.zero_grad()
+        # calculate feature exposures of current linear neutralization
+        exps = exposures(feats, pred[:,None]-model(feats))
+
+        # loss is positive when any exposures exceed their target
+        loss = (F.relu(F.relu(exps)-F.relu(targ_exp)) + F.relu(F.relu(-exps)-F.relu(-targ_exp))).sum()
+        #print(loss)
+        print(f'       loss: {loss:0.7f}', end='\r')
+
+        if loss < 1e-7: #7
+            #print('11111')
+            neutralizer = [p.detach().numpy() for p in model.parameters()]
+            neutralized_pred = pred[:,None]-model(feats)
+            break
+        loss.backward()
+        optimizer.step()
+    return neutralized_pred, neutralizer
+
+
+
+def reduce_all_exposures(df, column, neutralizers=[],
+                                     normalize=True,
+                                     gaussianize=True,
+                                     era_col="era",
+                                     max_exp=0.10):
+  
+    unique_eras = df[era_col].unique()
+    computed = []
+    for u in unique_eras:
+        print(u, '\r') #print era
+        df_era = df[df[era_col] == u]
+        scores = df_era[column].values #preds
+        exposure_values = df_era[neutralizers].values #features
+        
+        if normalize:
+            scores2 = []
+            for x in scores.T:
+                x = (scipy.stats.rankdata(x, method='ordinal') - .5) / len(x)
+                if gaussianize:
+                    x = scipy.stats.norm.ppf(x)
+                scores2.append(x)
+            scores = np.array(scores2)[0]
+
+        scores, neut = reduce_exposure(scores, exposure_values, max_exp)
+
+        scores /= scores.std()
+
+        computed.append(scores.detach().numpy())
+
+    return pd.DataFrame(np.concatenate(computed), columns=column, index=df.index)
+
+
+def neutralize_by_threshold(df, column, neutralizers=[],
+                                     normalize=True,
+                                     gaussianize=True,
+                                     era_col="era",
+                                     max_exp=0.10):
+  
+
+  data_rfe = reduce_all_exposures(df, column, neutralizers, 
+                                  normalize, gaussianize, 
+                                  era_col="era", 
+                                  max_exp=0.10)
+  
+  df[column] = data_rfe[column]
+  df[column]  -= df[column] .min()
+  df[column]  /= df[column] .max()
+
+  return df[column]
+
+
+
+
+
 
 
 def _neutralize(df, columns, by, ml_model, proportion=1.0): #['preds'], features,
